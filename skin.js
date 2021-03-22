e041faa89c704d8d0e59dba6dda1268112ce9c47
iflycn <iflycn@gmail.com>
2021-03-22 10:10:01 +0800

    新增御花园

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.2.2/15983
 // Filename: simplex_v5_autohide_ha.ggsk
-// Generated 周五 三月 19 10:49:41 2021
+// Generated 周五 三月 19 21:34:29 2021
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
